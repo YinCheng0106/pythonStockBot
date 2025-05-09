0b3abb1a109c7cc7a9c1de67e08e050dc83d280b
YinCheng <eason010630@gmail.com>
2025-05-09 19:45:23 +0800

    refactor: Stock query command, remove unnecessary code, improve error handling logic

--- src/commands/stock.py
+++ src/commands/stock.py
@@ -9,60 +9,49 @@ class Stock(commands.Cog):
     def __init__(self, client: commands.Bot):
         self.client = client
 
-    @app_commands.command(name="apple", description="Sends a list of stocks")
-    async def apple(self, interaction: discord.Interaction):
-        apple = yf.Ticker("AAPL")
-        data = apple.history(period="1d")
-        price = data['Close'].iloc[-1]
-        print(data)
-        await interaction.response.send_message(
-            f"Apple stock data: {price:.2f}",
-            ephemeral=True
-        )
-
     @app_commands.command(name="stock", description="searches for a stock")
     @app_commands.describe(symbol="The stock to search for")
     async def stock(self, interaction: discord.Interaction, symbol: str):
-        try:
-            if not symbol.isalpha():
-                await interaction.response.send_message("請輸入有效的股票代碼。", ephemeral=True)
-                return
-
-            stock = yf.Ticker(symbol)
-            data = stock.history(period="1mo")
-            if data.empty:
-                await interaction.response.send_message("股票數據下載失敗或沒有數據。", ephemeral=True)
-                return
-        except Exception as e:
-            await interaction.response.send_message(f"發生錯誤: {str(e)}", ephemeral=True)
-            return
+        await interaction.response.defer(ephemeral=True)
         stock = yf.Ticker(symbol)
-        data = stock.history(period="1mo")
-        price = data['Close'].iloc[-1]
-        if data.empty:
-            await interaction.response.send_message("股票數據下載失敗或沒有數據。", ephemeral=True)
-            return
-    
-        price = data['Close'].iloc[-1]
+        if(stock.info['quoteType'] == 'NONE'):
+            await interaction.followup.send(
+                "## 股票代碼錯誤或不存在",
+                ephemeral=True
+            )
+        else:
+            data = stock.history(period="1mo")
+            price = data['Close'].iloc[-1]
+
+            if data.empty:
+                await interaction.followup.send(
+                    "## 股票數據下載失敗或沒有數據",
+                    ephemeral=True
+                )
+                return
         
-        buf = BytesIO()
-        mpf.plot(data, type='candle', style='charles', title=symbol, volume=True, savefig=dict(fname=buf, dpi=100, bbox_inches='tight'))
-        buf.seek(0)
+            price = data['Close'].iloc[-1]
+            
+            buf = BytesIO()
+            mpf.plot(data, type='candle', style='charles', title=symbol, volume=True, savefig=dict(fname=buf, dpi=100, bbox_inches='tight'))
+            buf.seek(0)
 
-        file = discord.File(buf, filename=f"{symbol}_chart.png")
+            file = discord.File(buf, filename=f"{symbol}_chart.png")
 
-        stock_embed = discord.Embed(
-            title=f"{symbol} 股票資訊",
-            description=f"價格: {price:.2f}",
-            color=discord.Color.blue()
-        )
-        stock_embed.set_image(url=f"attachment://{symbol}_chart.png")
+            stock_embed = discord.Embed(
+                title=f"{symbol}",
+                description=f"價格: {price:.2f}",
+                color=discord.Color.blue()
+            )
+            stock_embed.set_image(url=f"attachment://{symbol}_chart.png")
+            stock_embed.set_footer(text="數據來源: Yahoo Finance")
+            stock_embed.set_author(name="股票查詢", url="https://tw.stock.yahoo.com")
 
-        await interaction.response.send_message(
-            embed=stock_embed,
-            file=file,
-            ephemeral=True
-        )
+            await interaction.followup.send(
+                embed=stock_embed,
+                file=file,
+                ephemeral=True
+            )
         
 
 async def setup(client: commands.Bot):
