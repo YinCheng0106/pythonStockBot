94e80c097a6e79805145a551991d6cc04024fc14
YinCheng <eason010630@gmail.com>
2025-05-12 10:10:58 +0800

    feat: add timestamp in stock's embed footer

--- src/commands/stock.py
+++ src/commands/stock.py
@@ -4,6 +4,7 @@ import mplfinance as mpf
 from discord.ext import commands
 from discord import app_commands
 from io import BytesIO
+from datetime import datetime
 
 class Stock(commands.Cog):
     def __init__(self, client: commands.Bot):
@@ -25,7 +26,7 @@ class Stock(commands.Cog):
         app_commands.Choice(name="最久", value="max")
     ])
     async def stock(self, interaction: discord.Interaction, symbol: str, period: str):
-        await interaction.response.defer(ephemeral=True)
+        await interaction.response.defer(ephemeral=False)
         stock = yf.Ticker(symbol)
         try:
             if(stock.info['quoteType'] == 'NONE'):
@@ -37,10 +38,37 @@ class Stock(commands.Cog):
                 data = stock.history(period=f"{period}")
                 price = data['Close'].iloc[-1]
                 name = stock.info['longName']
+                range = ""
                 open_price = data['Open'].iloc[0]
                 high_price = data['High'].max()
                 low_price = data['Low'].min()
                 type = stock.info['quoteType']
+                updateTime = datetime.fromtimestamp(stock.info['regularMarketTime'])
+
+                if period == "1d":
+                    range = "1日"
+                elif period == "5d":
+                    range = "5日"
+                elif period == "1mo":
+                    range = "1個月"
+                elif period == "3mo":
+                    range = "3個月"
+                elif period == "6mo":
+                    range = "6個月"
+                elif period == "1y":
+                    range = "1年"
+                elif period == "2y":
+                    range = "2年"
+                elif period == "5y":
+                    range = "5年"
+                elif period == "10y":
+                    range = "10年"
+                elif period == "ytd":
+                    range = "整年度"
+                elif period == "max":
+                    range = "最久"
+                else:
+                    range = "未知"
 
                 if data.empty:
                     await interaction.followup.send(
@@ -56,13 +84,14 @@ class Stock(commands.Cog):
                 file = discord.File(buf, filename=f"{symbol}_chart.png")
 
                 stock_embed = discord.Embed(
-                    title=f"{name} ({symbol})",
-                    description=f"貨幣：{stock.info['currency']}",
-                    color=discord.Color.blue()
+                    title = f"{name} ({symbol})",
+                    description = f"貨幣：{stock.info['currency']}",
+                    color = discord.Color.blue(),
+                    timestamp = updateTime
                 )
                 stock_embed.set_image(url=f"attachment://{symbol}_chart.png")
-                stock_embed.set_footer(text="數據來源: Yahoo Finance")
-                stock_embed.set_author(name="股票查詢", url="https://tw.stock.yahoo.com")
+                stock_embed.set_footer(text="數據來源: Yahoo Finance 更新時間")
+                stock_embed.set_author(name=f"股票查詢 ({range})", url="https://tw.stock.yahoo.com")
                 stock_embed.add_field(name="開盤價", value=f"{open_price:.2f}", inline=True)
                 stock_embed.add_field(name="最高價", value=f"{high_price:.2f}", inline=True)
                 stock_embed.add_field(name="最低價", value=f"{low_price:.2f}", inline=True)
@@ -73,7 +102,6 @@ class Stock(commands.Cog):
                 await interaction.followup.send(
                     embed=stock_embed,
                     file=file,
-                    ephemeral=True
                 )
         except Exception as e:
             await interaction.followup.send(
